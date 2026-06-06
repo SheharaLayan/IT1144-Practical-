2c3dd0c808540fced86ca711386d21e052bfd44e
K.S.S.Layan <134819382+SheharaLayan@users.noreply.github.com>
2026-06-06 09:43:47 +0530

    Add files via upload

---
+++ HTML+CSS+JavaScrpit Ex.3/script.js
@@ -0,0 +1,8 @@
+function displayName(){
+	let name=document.getElementById("name").value;
+	document.getElementById("result").innerText="Hello " + name;
+}
+document.getElementById("colorbtn");
+addEventListener("click",function(){
+	document.body.style.backgroundColor="yellow";
+});
